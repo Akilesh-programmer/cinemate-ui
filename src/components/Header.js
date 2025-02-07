cb0874c9437816b8c25e2948c6a6c28c39e04624
Akilesh <akileshakileshs1234@gmail.com>
2025-02-07 21:28:23 +0530

    Finished the header along with dark mode integration

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,34 @@ import { useState } from "react";
 import Logo from"../assets/images/favicon.png";
 import SearchImage from "../assets/images/search.svg";
 import DarkModeImage from "../assets/images/dark.svg";
+import LightModeImage from "../assets/images/light.svg";
 import MenuImage from "../assets/images/menu.svg";
 
 export const Header = () => {
     const [dropDownActive, setDropDownActive] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
 
     function DropMenu() {
         setDropDownActive(!dropDownActive);
     }
 
+    function ToggleTheme() {
+        const rootElement = document.getElementById("root");
+        const existing_class = rootElement.className;
+        
+        if(existing_class) {
+            rootElement.className = "";
+        } else {
+            rootElement.className = "dark";
+        }
+
+        setDarkMode(!darkMode);
+    }
+
     return (
-        <header>
-            <nav className="h-20 flex border-b-2">
+        <header className="dark:bg-gray-800">
+            <nav className="h-20 flex border-b-2 dark:bg-gray-800
+            dark:text-slate-300">
                 <div id="mobileNav"
                 className="sm:hidden flex flex-wrap items-center justify-between
                 w-11/12 mx-auto">
@@ -27,15 +43,21 @@ export const Header = () => {
                         
                     <div className="flex justify-between gap-5">
                         <button
-                        className="border-2 rounded-lg">
-                            <img className="w-8" src={DarkModeImage} alt="" />
+                        className="border-2 rounded-lg dark:border-gray-500"
+                        onClick={() => ToggleTheme()}>
+                            {darkMode ?
+                            <img className="w-8" src={LightModeImage} alt="" />: 
+                            <img className="w-8" src={DarkModeImage} alt="" />}
+                            
                         </button>
                         <button className="focus:bg-slate-200 focus:border-2
-                        focus:rounded-lg" onClick={() => DropMenu()}>
+                        focus:rounded-lg dark:focus:bg-slate-700
+                        dark:border-gray-500" onClick={() => DropMenu()}>
                             <img className="w-8" src={SearchImage} alt="" />
                         </button>
                         <button className="focus:bg-slate-200 focus:border-2
-                        focus:rounded-lg" onClick={() => DropMenu()}>
+                        focus:rounded-lg dark:focus:bg-slate-700
+                        dark:border-gray-500" onClick={() => DropMenu()}>
                             <img className="w-8" src={MenuImage} alt="" />
                         </button>
                     </div>
@@ -43,33 +65,38 @@ export const Header = () => {
 
                 <div id="mobileDropDown" 
                 className={`${dropDownActive ? "" : "hidden"}
-                absolute top-24 w-[100%] h-60 border-b-2 flex-col z-50
-                sm:hidden`} >
+                absolute top-20 w-[100%] h-60 border-b-2 flex-col z-50
+                sm:hidden dark:bg-gray-800`} >
                     <ul className="mx-auto text-center h-[100%] flex pb-4
                     flex-col justify-between text-lg">
                         <li>
                             <input type="text"
-                            className="border-2 rounded-lg w-11/12"
+                            className="border-2 rounded-lg w-11/12
+                            dark:bg-slate-700"
                             placeholder="   Search..."/>
                         </li>
                         <li>
                             <button 
-                            className="w-11/12 py-2 focus:bg-blue-500">
+                            className="w-11/12 py-2 focus:bg-blue-500
+                            hover:bg-blue-500">
                             Home</button>
                         </li>
                         <li>
                             <button 
-                            className="w-11/12 py-2 focus:bg-blue-500">
+                            className="w-11/12 py-2 focus:bg-blue-500
+                            hover:bg-blue-500">
                             Popular</button>
                         </li>
                         <li>
                             <button 
-                            className="w-11/12 py-2 focus:bg-blue-500">
+                            className="w-11/12 py-2 focus:bg-blue-500
+                            hover:bg-blue-500">
                             Top Rated</button>
                         </li>
                         <li>
                             <button 
-                            className="w-11/12 py-2 focus:bg-blue-500">
+                            className="w-11/12 py-2 focus:bg-blue-500
+                            hover:bg-blue-500">
                             Upcoming</button>
                         </li>
                     </ul>
@@ -115,11 +142,16 @@ export const Header = () => {
 
                     <div className="flex items-center text-xl gap-3">
                         <button
-                        className="border-2 rounded-lg">
-                            <img className="w-8" src={DarkModeImage} alt="" />
+                        className="border-2 rounded-lg dark:border-gray-500"
+                        onClick={() => ToggleTheme()}>
+                            {darkMode ? 
+                            <img className="w-8" src={LightModeImage} alt="" />: 
+                            <img className="w-8" src={DarkModeImage} alt="" />}
+                            
                         </button>
                         <input type="text" placeholder="   Search..."
-                        className="border-2 rounded-lg"/>
+                        className="border-2 rounded-lg dark:bg-slate-700
+                        dark:border-gray-500"/>
                     </div>
                 </div>
             </nav>
